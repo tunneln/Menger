c41e7d3307f993deb5ce545a87bfa8e6d6fb13c9
Noel Negusse <noel.blob@gmail.com>
2016-10-15 22:34:41 -0500

    Aded more viewing and camera stuff

--- src/camera.cc
+++ src/camera.cc
@@ -7,8 +7,52 @@ namespace {
 	float zoom_speed = 0.1f;
 };
 
-// FIXME: Calculate the view matrix
+void Camera::yaw(float dir) {
+	rotate = glm::rotate(rotation_speed*dir, up_) * rotate;
+	glm::vec3 eye(rotate * glm::vec4(eye_, 1));
+	look_ = glm::normalize(middle_ - eye);
+	right_ = glm::normalize(glm::cross(look_, up_));
+	up_ = glm::normalize(glm::cross(right_, look_));
+}
+
+void Camera::pitch(float dir) {
+	rotate = glm::rotate(rotation_speed*dir, right_) * rotate;
+	glm::vec3 eye(rotate * glm::vec4(eye_, 1));
+	look_ = glm::normalize(middle_ - eye);
+	right_ = glm::normalize(glm::cross(look_, up_));
+	up_ = glm::normalize(glm::cross(right_, look_));
+}
+
+void Camera::roll(float dir) {
+	glm::mat4 rollRotate = glm::rotate(dir * roll_speed, look_);
+	right_ = glm::normalize(glm::vec3(rollRotate * glm::vec4(right_, 0.0f)));
+	up_ = glm::normalize(glm::vec3(rollRotate * glm::vec4(up_, 0.0f)));
+}
+
+void Camera::trans(glm::vec2 dir) {
+	translate  = glm::translate((dir.y * up_ + dir.x * right_) * pan_speed) * translate;
+}
+
+void Camera::zoom(float dir) {
+	camera_distance_ += dir*zoom_speed;
+	eye_ = glm::vec3(0.0f, 0.0f, camera_distance_);
+}
+
 glm::mat4 Camera::get_view_matrix() const
 {
-	return glm::mat4();
+	glm::vec3 newEye(translate * rotate * glm::vec4(eye_, 1));
+	glm::vec3 newmiddle(translate * rotate * glm::vec4(middle_, 1));
+
+	glm::vec3 Z = glm::normalize(newEye - newmiddle);
+	glm::vec3 X = glm::cross(up_, Z);
+	glm::vec3 Y = glm::normalize(glm::cross(Z, X));
+	X = glm::normalize(X);
+
+	glm::mat4 eye(1.0f);
+	eye[0] = glm::vec4(X.x, Y.x, Z.x, 0);
+	eye[1] = glm::vec4(X.y, Y.y, Z.y, 0);
+	eye[2] = glm::vec4(X.z, Y.z, Z.z, 0);
+	eye[3] = glm::vec4(glm::dot(-X, newEye), glm::dot(-Y, newEye), glm::dot(-Z, newEye), 1);
+
+	return eye;
 }
--- src/camera.h
+++ src/camera.h
@@ -6,13 +6,21 @@
 class Camera {
 public:
 	glm::mat4 get_view_matrix() const;
-	// FIXME: add functions to manipulate camera objects.
+	void zoom(float dir);
+	void trans(glm::vec2 dir);
+	void yaw(float dir);
+	void pitch(float dir);
+	void roll(float dir);
 private:
 	float camera_distance_ = 3.0;
 	glm::vec3 look_ = glm::vec3(0.0f, 0.0f, -1.0f);
 	glm::vec3 up_ = glm::vec3(0.0f, 1.0, 0.0f);
 	glm::vec3 eye_ = glm::vec3(0.0f, 0.0f, camera_distance_);
-	// Note: you may need additional member variables
+	glm::vec3 middle_ = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 right_ = glm::vec3(0.0f, 0.0f, 0.0f);
+
+	glm::mat4 translate = glm::mat4(1.0f);
+	glm::mat4 rotate = glm::mat4(1.0f);
 };
 
 #endif
